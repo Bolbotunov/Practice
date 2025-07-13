636d9a107a30bedf43df29e421b3d22f04641058
Bolbotunov Konstantin <100226065+Bolbotunov@users.noreply.github.com>
2025-07-13 14:37:17 +0300

    fix: default state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import "./App.css";
-import TestComponent from "./components/TestComponent";
 
 function App() {
-  return (
-    <>
-      <TestComponent />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
