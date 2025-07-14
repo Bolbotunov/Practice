bfa596dc37ba7c42322a0b517e6c5fd584035dc6
Bolbotunov Konstantin <100226065+Bolbotunov@users.noreply.github.com>
2025-07-14 16:34:59 +0300

    feat: practice render fetch res

--- src/components/UseAnotherChild.tsx
+++ src/components/UseAnotherChild.tsx
@@ -1,18 +1,50 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function UseAnotherChild() {
-  const NUMBER = 15;
+  const [state, setState] = useState("");
+  const [query, setQuery] = useState("");
+  const url = "https://jsonplaceholder.typicode.com/todos/1";
+  const newUrl = "https://jsonplaceholder.typicode.com/todos/2";
+  const NUMBER = 5;
 
   function createArr() {
-    let array = Array.from({ length: NUMBER }, (_, i) => i + 1);
-    return array;
+    return Array.from({ length: NUMBER }, (_, i) => i + 1);
   }
+  useEffect(() => {
+    console.log("компонент смонтирован");
+    getData(url);
+    return () => console.log("компонент размонтирован");
+  }, []);
+
+  async function getData(url: string) {
+    try {
+      console.log("req:", state);
+      let promise = await fetch(url);
+      let data = await promise.json();
+      console.log(data);
+    } catch (err) {
+      console.log("err", err);
+    }
+  }
+
+  function queryHandler(e: string) {
+    console.log("query input", e);
+    setQuery(e);
+  }
+
+  useEffect(() => {
+    getData(newUrl);
+  }, [query]);
 
   return (
     <div className="list">
+      <input
+        placeholder="inp"
+        onChange={(e) => queryHandler(e.target.value)}
+      ></input>
       <ul>
         {createArr().map((item, index) => (
-          <li>{item}</li>
+          <li key={index}>{item}</li>
         ))}
       </ul>
     </div>
